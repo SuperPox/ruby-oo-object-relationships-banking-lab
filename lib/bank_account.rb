198cb41e0e9b795d8c327fe58d4d323cba2601e1
SuperPox <calebkohl@gmail.com>
2021-01-26 16:11:34 -0700

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,31 @@
+require 'pry'
 class BankAccount
+    attr_reader :name
+    attr_accessor :balance, :status
 
+    def initialize(name)
+        @name = name
+        @balance = 1000
+        @status = "open"
+    end
+
+    def deposit(amount)
+        self.balance = self.balance + 1000
+    end
+
+    def display_balance
+        "Your balance is $#{self.balance}."
+    end
+
+    def valid? #wants a bool
+        if self.balance > 0 && self.status == "open"
+            return true
+        else
+            return false
+        end
+    end
+
+    def close_account
+        self.status = "closed"
+    end
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,40 @@
+require 'pry'
 class Transfer
-  # your code here
+  attr_accessor :sender, :receiver, :status, :amount, :lasttransfer
+
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    @amount = 50
+    @lasttransfer = 0
+  end
+
+  def valid?
+    #binding.pry
+    if @sender.valid? == true && @receiver.valid? == true
+      return true
+    else
+      return false
+    end
+  end
+
+  def execute_transaction
+    if self.status == "pending" && @sender.balance > self.amount && @sender.status == "open" && @receiver.status == "open"
+      @sender.balance = @sender.balance - self.amount
+      @receiver.balance = @receiver.balance + self.amount
+      @lasttransfer = self.amount
+      self.status = "complete"
+    else
+      self.status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    end
+  end
+  
+  def reverse_transfer
+
+  end
+
+
 end
